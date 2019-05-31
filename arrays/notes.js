9cf5f3b4ed3f73ff25b8f24090a61750ecb2b854
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-05-31 15:47:00 +0100

    arrays part1

---
+++ arrays/notes.js
@@ -0,0 +1,48 @@
+const notes = ['Note 1','Note 2','Note 3','Note 4'];
+
+console.log(notes.length); // 4
+console.log(notes[1]); // Note 2
+console.log(notes[notes.length - 1]); // Note 4 (last item)
+
+// ----- END -----
+notes.push('Note 5'); // add the new value at the end of arrray
+notes.pop() // remove last item from the array and return the deleted value
+console.log(notes);
+
+// ----- BEGIN -----
+notes.shift(); // remove first item from the array and return the deleted value
+notes.unshift('New note'); // add the new value at the begining of arrray
+console.log(notes); 
+
+// ----- ANYWHERE -----
+
+// REMOVE
+notes.splice(1, 2); // (starting point, how many items)
+console.log(notes); 
+
+// ADD
+notes.splice(1, 0, 'New item'); // (starting point, remove none, insert item)
+console.log(notes); 
+
+// Looping through array
+// a calback function is a function that is called by another function
+
+notes.forEach(function (item, index) {
+    console.log(index);
+    console.log(item);
+});
+
+// ([initialization]; [condition]; [final-expression])
+for (let i = 0; i <= 5; i++) {
+    console.log(`Counting... ${i}`);
+}
+
+for (let i = 0; i <= notes.length; i++){
+    console.log(notes[i]);
+}
+
+// loop in reverse
+
+for (let i = notes.length - 1; i >= 0; i--) {
+    console.log(notes[i]);
+}
---
+++ arrays/todo.js
@@ -0,0 +1,16 @@
+const todos = ['Go to gym', 'Clean the house', 'Finish the book', 'Cut hair', 'Call sister'];
+
+console.log(`You have ${todos.length} todos.`);
+console.log(todos);
+
+todos.splice(2, 1);
+todos.push('Clean car');
+todos.shift();
+
+todos.forEach(function (item, index) {
+    console.log(`${index + 1}. ${item}`);
+});
+
+for (let i = 0; i <= todos.length; i++) {
+    console.log(todos[i]);
+}
