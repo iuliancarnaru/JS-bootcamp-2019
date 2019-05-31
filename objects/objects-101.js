a3f09e630c8eb755a21588d842a00137ecc39633
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-05-31 09:56:58 +0100

    objects part1

---
+++ objects/objects-101.js
@@ -0,0 +1,27 @@
+// Object has properties (key - value pair)
+
+let myBook = {
+    title: "A New Earth",
+    author: "Eckhart Tolle",
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`);
+
+myBook.title = "The Power of Now";
+
+console.log(`${myBook.title} by ${myBook.author}`);
+
+// ----- CHALLENGE -----
+
+let person = {
+    name: "Iulian Carnaru",
+    age: 34,
+    location: "London"
+}
+
+console.log(`Hi, I'm ${person.name}. I'm ${person.age} years old and I live in ${person.location}.`);
+
+person.age = person.age + 1;
+
+console.log(`This year I will be ${person.age} years old.`);
---
+++ objects/objects-functions.js
@@ -0,0 +1,41 @@
+
+let myBook = {
+    title: "A New Earth",
+    author: "Eckhart Tolle",
+    pageCount: 326
+}
+
+let otherBook = {
+    title: "The Power of Now",
+    author: "Eckhart Tolle",
+    pageCount: 236
+}
+
+let getSummary = function (book) {
+    return {
+        description: `${book.title} by ${book.author}.`,
+        pageCountSummary: `${book.title} is ${book.pageCount} pages long.`
+    }
+}
+
+let bookSummary = getSummary(myBook);
+let otherBookSummary = getSummary(otherBook);
+
+console.log(otherBookSummary.description);
+console.log(bookSummary.pageCountSummary);
+
+// ----- CHALLENGE -----
+
+const tempConvertor = function (tempFarenheit) {
+    let celsius = (tempFarenheit - 32) * 5 / 9;
+    let kelvin = (tempFarenheit + 459.67) * 5 / 9;
+
+    return {
+        tempInCelsius: `${tempFarenheit} degree fahrenheit is ${celsius} degree celsius`,
+        tempInKelvin: `${tempFarenheit} degree fahrenheit is ${kelvin} degree kelvin`
+    }
+}
+
+let tempValues = tempConvertor(50);
+console.log(tempValues.tempInCelsius);
+console.log(tempValues.tempInKelvin);
