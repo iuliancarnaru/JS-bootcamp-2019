da574548b5c152a8662dc7a0eb93174ed92ac805
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-04 20:59:55 +0100

    todos update

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,46 +1,41 @@
 const notes = [
-    {
-      title: "My next trip",
-      body: "I would like to go to Spain."
-    },
-    {
-      title: "Habbits to work on",
-      body: "Eat better, sleep earlier."
-    },
-    {
-      title: "Office modification",
-      body: "Get a new 4k monitor"
-    }
+  {
+    title: "My next trip",
+    body: "I would like to go to Spain."
+  },
+  {
+    title: "Habbits to work on",
+    body: "Eat better, sleep earlier."
+  },
+  {
+    title: "Office modification",
+    body: "Get a new 4k monitor"
+  }
 ];
-  
-const filters = {
-  searchText: ''
-}
 
-const renderNotes = function (notes, filters) {
-  const filteredNotes = notes.filter(function (note) {
+const filters = {
+  searchText: ""
+};
+
+const renderNotes = function(notes, filters) {
+  const filteredNotes = notes.filter(function(note) {
     return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
   });
 
-  document.querySelector('#notes').innerHTML = '';
-  
-  filteredNotes.forEach(function (note) {
-    const noteElement = document.createElement('p');
+  document.querySelector("#notes").innerHTML = "";
+
+  filteredNotes.forEach(function(note) {
+    const noteElement = document.createElement("p");
     noteElement.textContent = note.title;
-    document.querySelector('#notes').appendChild(noteElement);
-  })
-}
+    document.querySelector("#notes").appendChild(noteElement);
+  });
+};
 
 renderNotes(notes, filters);
 
-document.querySelector('#search-text').addEventListener('input', function (event) {
-  filters.searchText = event.target.value;
-  renderNotes(notes, filters);
-});
-
-document.querySelector('#name-form').addEventListener('submit', function (event) {
-  event.preventDefault();
-  console.log(event.target.elements.firstName.value);
-  event.target.elements.firstName.value = '';
-});
-
+document
+  .querySelector("#search-text")
+  .addEventListener("input", function(event) {
+    filters.searchText = event.target.value;
+    renderNotes(notes, filters);
+  });
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,63 +1,80 @@
 const todos = [
-    {
-      text: "Go to gym",
-      completed: false
-    },
-    {
-      text: "Clean the house",
-      completed: true
-    },
-    {
-      text: "Finish the book",
-      completed: true
-    },
-    {
-      text: "Cut hair",
-      completed: false
-    }
+  {
+    text: "Go to gym",
+    completed: false
+  },
+  {
+    text: "Clean the house",
+    completed: true
+  },
+  {
+    text: "Finish the book",
+    completed: true
+  },
+  {
+    text: "Cut hair",
+    completed: false
+  }
 ];
 
 const filters = {
-  searchText:''
-}
+  searchText: "",
+  hideCompleted: false
+};
 
-const renderTodos = function (todos, filters) {
-  const filterTodos = todos.filter(function (todo) {
-    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+const renderTodos = function(todos, filters) {
+  const filterTodos = todos.filter(function(todo) {
+    const searchTextMatch = todo.text
+      .toLowerCase()
+      .includes(filters.searchText.toLowerCase());
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+    return searchTextMatch && hideCompletedMatch;
   });
 
-  const incompleteTodos = filterTodos.filter(function (todo) {
-    return !todo.completed
+  const incompleteTodos = filterTodos.filter(function(todo) {
+    return !todo.completed;
   });
 
-  document.querySelector('#todos').innerHTML = '';
+  document.querySelector("#todos").innerHTML = "";
 
-  const summary = document.createElement('h2');
+  const summary = document.createElement("h2");
   summary.textContent = `You have ${incompleteTodos.length} todos.`;
-  document.querySelector('#todos').appendChild(summary);
- 
-  filterTodos.forEach(function (todo) {
-    const todoElement = document.createElement('p');
+  document.querySelector("#todos").appendChild(summary);
+
+  filterTodos.forEach(function(todo) {
+    const todoElement = document.createElement("p");
     todoElement.textContent = todo.text;
-    document.querySelector('#todos').appendChild(todoElement);
+    document.querySelector("#todos").appendChild(todoElement);
   });
-}
+};
 
 renderTodos(todos, filters);
 
-document.querySelector('#search-text').addEventListener('input', function (event) {
-  filters.searchText = event.target.value;
-  renderTodos(todos, filters);
-});
-
-document.querySelector('#todo-form').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  todos.push({
-    text: event.target.elements.todoText.value,
-    completed: false
+document
+  .querySelector("#search-text")
+  .addEventListener("input", function(event) {
+    filters.searchText = event.target.value;
+    renderTodos(todos, filters);
   });
 
-  event.target.elements.todoText.value = '';
-  renderTodos(todos, filters);
-});
+document
+  .querySelector("#todo-form")
+  .addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    todos.push({
+      text: event.target.elements.todoText.value,
+      completed: false
+    });
+
+    event.target.elements.todoText.value = "";
+    renderTodos(todos, filters);
+  });
+
+document
+  .querySelector("#hide-completed")
+  .addEventListener("change", function(event) {
+    filters.hideCompleted = event.target.checked;
+    renderTodos(todos, filters);
+  });
