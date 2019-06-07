020e204e180f75989c8760376dee3283f5595da2
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-06-07 08:44:28 +0100

    sorting options

--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -44,13 +44,47 @@ const generateNoteDOM = function(note) {
   return noteElement;
 };
 
-// Sort notes
-const sortNotes = function () {
-  
-}
+// Sort your notes by one of the three options
+const sortNotes = function(notes, sortBy) {
+  if (sortBy === "byEdited") {
+    return notes.sort(function(a, b) {
+      if (a.updatedAt > b.updatedAt) {
+        return -1;
+      } else if (a.updatedAt < b.updatedAt) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === "byCreated") {
+    return notes.sort(function(a, b) {
+      if (a.createdAt > b.createdAt) {
+        return -1;
+      }
+      if (a.createdAt < b.createdAt) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === "alphabetical") {
+    return notes.sort(function(a, b) {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1;
+      } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else {
+    return notes;
+  }
+};
 
 // REnder application notes
 const renderNotes = function(notes, filters) {
+  notes = sortNotes(notes, filters.sortBy);
   const filteredNotes = notes.filter(function(note) {
     return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
   });
@@ -66,7 +100,6 @@ const saveNotes = function(notes) {
 };
 
 // Generate last edited
-const generateLastEdited = function (timestamp) {
-  return `Last edited ${moment(timestamp).fromNow()}`
-}
-
+const generateLastEdited = function(timestamp) {
+  return `Last edited ${moment(timestamp).fromNow()}`;
+};
