9ce9b2c8ac9f0c7ccbb0ef12a95570eef0810113
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-09 12:33:25 +0100

    converting to class

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,72 +1,70 @@
 // CONTROLER
-
-const Hangman = function(word, remaningGuesses) {
-  this.word = word.toLowerCase().split("");
-  this.remaningGuesses = remaningGuesses;
-  this.guessedLetters = [];
-  this.status = "playing";
-};
-
-Hangman.prototype.getStatusMessage = function() {
-  if (this.status === "playing") {
-    return `Guesses left: ${this.remaningGuesses}`;
-  } else if (this.status === "failed") {
-    return `Nice try! The word was "${this.word.join("")}"`;
-  } else {
-    return `Great work! You guessed the word.`
-  }
-};
-
-Hangman.prototype.calculateStatus = function() {
-  // const lettersUnguessd = this.word.filter(letter => {
-  //   return !this.guessedLetters.includes(letter);
-  // });
-
-  // const finished = lettersUnguessd.length === 0
-
-  const finished = this.word.every(letter => {
-    return this.guessedLetters.includes(letter);
-  });
-
-  if (this.remaningGuesses === 0) {
-    this.status = "failed";
-  } else if (finished) {
-    this.status = "finished";
-  } else {
+class Hangman {
+  constructor(word, remaningGuesses) {
+    this.word = word.toLowerCase().split("");
+    this.remaningGuesses = remaningGuesses;
+    this.guessedLetters = [];
     this.status = "playing";
   }
-};
-
-Hangman.prototype.getPuzzle = function() {
-  let puzzle = "";
-
-  this.word.forEach(letter => {
-    if (this.guessedLetters.includes(letter) || letter === " ") {
-      puzzle += letter;
-    } else {
-      puzzle += "*";
+  getStatusMessage() {
+    if (this.status === "playing") {
+      return `Guesses left: ${this.remaningGuesses}`;
     }
-  });
-
-  return puzzle;
-};
-
-Hangman.prototype.makeGuess = function(guess) {
-  guess = guess.toLowerCase();
-  const isUnique = !this.guessedLetters.includes(guess);
-  const isBadGuess = !this.word.includes(guess);
-
-  if (this.status !== "playing") {
-    return;
+    else if (this.status === "failed") {
+      return `Nice try! The word was "${this.word.join("")}"`;
+    }
+    else {
+      return `Great work! You guessed the word.`;
+    }
   }
-
-  if (isUnique) {
-    this.guessedLetters.push(guess);
+  calculateStatus() {
+    // const lettersUnguessd = this.word.filter(letter => {
+    //   return !this.guessedLetters.includes(letter);
+    // });
+    // const finished = lettersUnguessd.length === 0
+    const finished = this.word.every(letter => {
+      return this.guessedLetters.includes(letter);
+    });
+    if (this.remaningGuesses === 0) {
+      this.status = "failed";
+    }
+    else if (finished) {
+      this.status = "finished";
+    }
+    else {
+      this.status = "playing";
+    }
   }
-
-  if (isUnique && isBadGuess) {
-    this.remaningGuesses--;
+  getPuzzle() {
+    let puzzle = "";
+    this.word.forEach(letter => {
+      if (this.guessedLetters.includes(letter) || letter === " ") {
+        puzzle += letter;
+      }
+      else {
+        puzzle += "*";
+      }
+    });
+    return puzzle;
   }
+  makeGuess(guess) {
+    guess = guess.toLowerCase();
+    const isUnique = !this.guessedLetters.includes(guess);
+    const isBadGuess = !this.word.includes(guess);
+    if (this.status !== "playing") {
+      return;
+    }
+    if (isUnique) {
+      this.guessedLetters.push(guess);
+    }
+    if (isUnique && isBadGuess) {
+      this.remaningGuesses--;
+    }
+    this.calculateStatus();
+  }
+}
 
-  this.calculateStatus();
-};
+
+
+
+
--- oop/person.js
+++ oop/person.js
@@ -1,28 +1,27 @@
 // CONSTRUCTOR FUNCTION (CF name must start with capital letter)
 // Prototypal inheritance
+class Person {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.likes = likes;
+  }
+  getBio() {
+    let bio = `${this.firstName} is ${this.age} years old.`;
+    this.likes.forEach(like => {
+      bio += ` ${this.firstName} likes ${like}.`;
+    });
+    return bio;
+  }
+  setName(fullName) {
+    const names = fullName.split(" ");
+    this.firstName = names[0];
+    this.lastName = names[1];
+  }
+}
 
-const Person = function(firstName, lastName, age, likes = []) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.likes = likes;
-};
 
-Person.prototype.getBio = function() {
-  let bio = `${this.firstName} is ${this.age} years old.`;
-
-  this.likes.forEach(like => {
-    bio += ` ${this.firstName} likes ${like}.`;
-  });
-
-  return bio;
-};
-
-Person.prototype.setName = function(fullName) {
-  const names = fullName.split(" ");
-  this.firstName = names[0];
-  this.lastName = names[1];
-};
 
 const me = new Person("Iulian", "Carnaru", 34, ["Reading", "Coding"]); // CF can be initialized with NEW keyword
 console.log(me.lastName);
