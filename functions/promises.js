cc7427cfbcf9f5c676d357f3fcc6779a5193c8ac
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-06-11 13:33:55 +0100

    promise intro

--- functions/promises.js
+++ functions/promises.js
@@ -17,14 +17,17 @@ getDataCallback((error, data) => {
 });
 
 // PROMISE
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // success
-    // resolve(`This is the promise data`);
-    // fail
-    reject(`Upps! Something happened!`);
-  }, 2000);
-});
+const getDataPromise = data =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // success
+      resolve(`This is the promise data`, data);
+      // fail
+      // reject(`Upps! Something happened!`);
+    }, 2000);
+  });
+
+const myPromise = getDataPromise({ name: "Iulian" });
 
 myPromise.then(
   data => {
