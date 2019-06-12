79ab57443a3ecb377f794c7185fd5e1e1a1b23da
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-06-12 12:52:28 +0100

    learning webpack

---
+++ boilerplate/src/index.js
@@ -0,0 +1,7 @@
+import { add, name } from './utilities';
+import { scream } from './functions';
+
+console.log(`index`);
+console.log(add(2, 3));
+
+console.log(scream(name));
---
+++ boilerplate/src/utilities.js
@@ -0,0 +1,8 @@
+// named export -- as many as needed
+// default export -- only one
+
+export const add = (a, b) => {
+    return a + b
+}
+
+export const name = "Iulian";
