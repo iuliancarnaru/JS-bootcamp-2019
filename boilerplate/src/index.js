55e69ccef8b2168688b5bd47af891f3ca968b220
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-06-12 13:58:21 +0100

    adding scss css support

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,1 +1,3 @@
 // import other JS here (entry point)
+
+console.log(`Application has started...`);
--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -7,11 +7,28 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
   },
   devServer: {
     contentBase: path.resolve(__dirname, "public"),
-    publicPath: '/scripts/'
+    publicPath: "/scripts/"
   },
-  devtool: 'source-map'
+  devtool: "source-map"
 };
